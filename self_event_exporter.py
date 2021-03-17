cad38472fffda3f89859dc93787f27456dfccb1b
Kiri Tachibana <kiritachibana2021@gmail.com>
2021-03-17 22:56:41 +0900

    First commit

---
+++ self_event_exporter.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+target_url = 'https://kakuyomu.jp/user_events/16816452218925948002?page='
+delim = "\t"
+
+header = ["Title","Author","URL","Genre","Status","Episodes","Characters"]
+print(delim.join(header))
+
+noMoreContent = False
+idx = 1
+while(noMoreContent == False):
+    r = requests.get(target_url + str(idx))
+    soup = BeautifulSoup(r.text, 'lxml')
+    if(soup.find("div", attrs={"class":"widget-emptyMessage"})):
+        exit()
+
+    for card in soup.find_all("div", attrs={"class": "enterdWork"}):
+        cols = list()
+
+        work = card.find("a", attrs={"class": "widget-workCard-titleLabel"})
+
+        cols.append(work.text) # Title
+        cols.append(card.find("a", attrs={"class": "widget-workCard-authorLabel"}).text) # Author
+        cols.append("https://kakuyomu.jp" + work.get("href")) # URL
+        cols.append(card.find(itemprop="genre").text) #genre
+
+        cols.append(card.find("span", attrs={"class": "widget-workCard-statusLabel"}).text) # Status
+        cols.append(card.find("span", attrs={"class": "widget-workCard-episodeCount"}).text.replace("話","")) # Number of episodes
+        cols.append(card.find("span", attrs={"class": "widget-workCard-characterCount"}).text.replace('文字',"").replace(',', "")) # Number of characters
+
+        print(delim.join(cols))
+    idx += 1
